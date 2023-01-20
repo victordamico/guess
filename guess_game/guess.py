83d507ede4e519aa91f813a93e33e4c85f70ed43
Victor D'Amico <105016790+victordamico@users.noreply.github.com>
2023-01-20 08:37:10 -0300

    Update guess.py

--- guess_game/guess.py
+++ guess_game/guess.py
@@ -24,8 +24,8 @@ bemvindo()
 
 #Nivel de Dificuldade Facil, Medio e Dificil
 
-nivel = input("Escolha a Dificuldade \033[32mFacil\033[32m, \033[33mMedio\033[33m, \033[35mDificil\033[35m:\n ").title()
-if nivel == "Facil":
+nivel = input("Escolha a Dificuldade \033[32mFacil\033[32m, \033[33mMedio\033[33m, \033[35mDificil\033[35m \033[37m:").title()
+if nivel == "Facil\033[37m":
     b = 5
     chances = 3
     print(f" Você escolheu o modo Fácil aqui voce tera que acertar um número de 1 a 5 \n Você tem {chances} Chances\n")
@@ -68,6 +68,7 @@ if chances == contador and numero_secreto != palpite:
     print(f' O número secreto era: {numero_secreto}\n')
     print(f' Sua Pontuação {pontos}p')
     line() 
+
 
 
 
